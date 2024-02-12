708f124a86ff42ed7204f27365594f3087cae1aa
alissonif <josealissonif@gmail.com>
2024-02-11 23:14:15 -0300

    install taiwndcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { Header } from '@/components/header';
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
+    <View className="flex-1 pt-8">
+      <Text className="text-white text-2xl font-heading"></Text>
+      <Header title='Faça seu pedido' cardQuantityItems={5} />
     </View>
   )
 }
